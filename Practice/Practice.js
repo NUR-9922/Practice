29f397b0aff65456c52fdb75461a10bfcd357605
NUSH-9922 <nush9922@gmail.com>
2023-10-07 16:28:05 +0530

    intial commit

---
+++ Practice/Practice.js
@@ -0,0 +1,15 @@
+
+// function check(word, reversedWord){
+//     reverseReversedWord = reversedWord.split("").reverse().join("")
+//     return word === reverseReversedWord ? "Yes! right" : "Noo!";
+// }
+// console.log(check("water", "retaw"));
+let Student = { Name : "Nur Alam Mondal",
+                Age : 18,
+                class : 12
+
+}
+
+console.log(`My Name is ${Student.Name} amd my age is ${Student.Age}, I study in class ${Student.class
+}`)
+// console.log(Student.Name)
